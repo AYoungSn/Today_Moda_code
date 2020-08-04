5938ffd28c4accfe62d098d9c89ef4cad8d567fd
ahnyoungseon99 <dudtjs0920@naver.com>
2020-08-04 16:12:08 +0900

    expo_version_weather

--- src/Weather.tsx
+++ src/Weather.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {StyleSheet, Text, View, Image, Dimensions} from 'react-native';
-import Icon from 'react-native-vector-icons/Ionicons';
+import { Ionicons } from '@expo/vector-icons';
 // https://ionicons.com/   icon 파일명 찾는곳
 
 import PropTypes from 'prop-types';
@@ -8,42 +8,42 @@ import PropTypes from 'prop-types';
 const weatherCases = {
     Thunderstorm: {
         colors: ['#415b72', '#004787'],
-        icon: 'ios-thunderstorm-outline',
+        icon: 'ios-thunderstorm',
         weatherName: 'Thunderstorm',
         desc: '오늘은 천둥 번개가 쳐요'    
     },
 
     Drizzle: {
         colors: ['#d7dde5', '#c9d2d8', '#5db7fc'],
-        icon: 'ios-rainy-outline',
+        icon: 'ios-rainy',
         weatherName: 'Drizzle',
         desc: '오늘은 이슬비가 내려요'
     },
 
     Rain: {
         colors: ['#00C6FB', '#005BEA'],
-        icon: 'ios-umbrella-outline',
+        icon: 'ios-umbrella',
         weatherName: 'Rain',
         desc: '오늘은 비가 내려요. 우산을 꼭 챙기세요!'
     },
 
     Snow: {
         colors: ['#7DE2FC', '#B9B6E5'],
-        icon: 'ios-snow-outline',
+        icon: 'ios-snow',
         weatherName: 'Snow',
         desc: '오늘은 눈이 내려요.'
     },
 
     Clear: {
         colors: ['#ffffff', '#6de1ff'],
-        icon: 'ios-sunny-outline',
+        icon: 'ios-sunny',
         weatherName: 'Clear',
         desc: '오늘은 맑아요'
     },
 
     Clouds: {
         colors: ['#d7dde5', '#646970'],
-        icon: 'ios-partly-sunny-outline',
+        icon: 'ios-cloudy',
         weatherName: 'Clouds',
         desc: '오늘은 흐려요'
     }
@@ -55,7 +55,7 @@ function Weather({ city, weatherName, temp, feels }){
     return (
         <View style={styles.container}>
             <View style={styles.weather_img}>
-                <Icon name={weatherCases[weatherName].icon} size={width/3} color="black"/>
+                <Ionicons name={weatherCases[weatherName].icon} size={width/3} color="black"/>
             </View>
             
             <View style={styles.w_info}>
